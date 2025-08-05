df571f4b7ee236b2bc2addc36c6af19c986d421f
Manish <manishkshyp0123@gmail.com>
2025-08-05 15:17:36 +0530

    script.js
    Signed-off-by: Manish <manishkshyp0123@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const certificates = [
-  'certificates/cert1.jpg',
-  'certificates/cert2.jpg',
-  'certificates/cert3.jpg',
-  'certificates/cert4.jpg',
-  'certificates/cert5.jpg',
-  'certificates/cert6.jpg',
-  'certificates/cert7.jpg',
-  'certificates/cert8.jpg'
+'certificates/cert1.jpg',
+'certificates/cert2.jpg',
+'certificates/cert3.jpg',
+'certificates/cert4.jpg',
+'certificates/cert5.jpg',
+'certificates/cert6.jpg',
+'certificates/cert7.jpg',
+'certificates/cert8.jpg'
 ];
 
 let currentCert = 0;
@@ -15,34 +15,34 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 
 function updateCertificate() {
-  certImg.classList.remove('fade-in');
-  setTimeout(() => {
-    certImg.src = certificates[currentCert];
-    certImg.alt = `Achievement Certificate ${currentCert + 1}`;
-    certImg.classList.add('fade-in');
-  }, 500);
+certImg.classList.remove('fade-in');
+setTimeout(() => {
+certImg.src = certificates[currentCert];
+certImg.classList.add('fade-in');
+}, 100);
 }
 
 function showPrev() {
-  currentCert = (currentCert - 1 + certificates.length) % certificates.length;
-  updateCertificate();
-  resetAutoSlide();
+currentCert = (currentCert - 1 + certificates.length) % certificates.length;
+updateCertificate();
+resetAutoSlide();
 }
 
 function showNext() {
-  currentCert = (currentCert + 1) % certificates.length;
-  updateCertificate();
-  resetAutoSlide();
+currentCert = (currentCert + 1) % certificates.length;
+updateCertificate();
+resetAutoSlide();
 }
 
 prevBtn.onclick = showPrev;
 nextBtn.onclick = showNext;
 
-let slideInterval = setInterval(showNext, 4000);
+let slideInterval = setInterval(showNext, 3000);
 
 function resetAutoSlide() {
-  clearInterval(slideInterval);
-  slideInterval = setInterval(showNext, 4000);
+clearInterval(slideInterval);
+slideInterval = setInterval(showNext, 3000);
 }
 
 updateCertificate();
+
